4850ca400bbb87670a2fd73c7f82782b6209ef6d
ahmed.gaber <ahmedgaberelgamaleg@gmail.com>
2020-12-18 20:12:48 +0200

    add authentication

--- django_simple_blog/urls.py
+++ django_simple_blog/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('simple_blog.urls'))
+    path('', include('simple_blog.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('membership.urls')),
 ]
--- simple_blog/views.py
+++ simple_blog/views.py
@@ -8,7 +8,7 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
-    ordering = ['-id']
+    ordering = ['-created_at']
 
 class ArticleDetailView(DetailView):
     model = Post
