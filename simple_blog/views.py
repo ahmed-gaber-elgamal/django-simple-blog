78cc22eba4d72490e28155b6a80279af53f577cc
ahmed.gaber <ahmedgaberelgamaleg@gmail.com>
2020-12-18 17:57:35 +0200

    add delete post

--- simple_blog/views.py
+++ simple_blog/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import *
 from .forms import *
+from django.urls import reverse_lazy
 
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
-
+    ordering = ['-id']
 
 class ArticleDetailView(DetailView):
     model = Post
@@ -18,10 +19,16 @@ class AddPostView(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'add_post.html'
-    # fields = '__all__'
+    fields = '__all__'
 
 
 class UpdatePostView(UpdateView):
     model = Post
     form_class = EditForm
     template_name = 'update_post.html'
+
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
