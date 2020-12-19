949cecb4cfc5711b359c63d9bb600de886f80adf
ahmed.gaber <ahmedgaberelgamaleg@gmail.com>
2020-12-19 19:23:13 +0200

    add profile page

--- membership/urls.py
+++ membership/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserRegistrationView, UserEditView, PasswordChangeView
+from .views import UserRegistrationView, UserEditView, PasswordChangeView, ProfilePageView
 from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('edit_profile/', UserEditView.as_view(), name='edit-profile'),
     path('password/', PasswordChangeView.as_view()),
+    path('<int:pk>/profile/', ProfilePageView.as_view(), name='profile-page'),
 ]
--- membership/views.py
+++ membership/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.urls import reverse_lazy
 from .forms import *
 from django.contrib.auth import views as auth_views
+from simple_blog.models import Profile
 
 class UserRegistrationView(generic.CreateView):
     form_class = SignUpForm
@@ -24,3 +25,15 @@ class PasswordChangeView(auth_views.PasswordChangeView):
     form_class = ChangePasswordForm
     template_name = 'registration/change_password.html'
     success_url = reverse_lazy('home')
+
+
+class ProfilePageView(generic.DetailView):
+    model = Profile
+    template_name = 'registration/profile.html'
+
+    def get_context_data(self, *args, **kwargs):
+        # users = Profile.objects.all()
+        context = super(ProfilePageView, self).get_context_data(*args, **kwargs)
+        page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+        context['page_user'] = page_user
+        return context
